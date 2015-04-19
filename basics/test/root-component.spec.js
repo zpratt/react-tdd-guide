75252ca71cb29bd3f3f41a4b0856c268ac6a67f3
zach pratt <zach@attackofzach.com>
2015-04-19 11:58:33 -0500

    step 1 - force it to be a react component - red

--- basics/test/root-component.spec.js
+++ basics/test/root-component.spec.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var RootComponent = require('../lib/root-component.jsx');
+var RootComponent = require('../lib/root-component.jsx'),
+
+    React = require('react');
 
 describe('Root Component', function () {
     beforeEach(function () {
-
+        var RootElement = React.createElement(RootComponent);
     });
 
     it('should be a unordered list', function () {
