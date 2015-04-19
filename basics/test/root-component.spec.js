52fa696754022217c761a93c32c15bb827e9b8c5
zach pratt <zach@attackofzach.com>
2015-04-19 11:56:17 -0500

    step 1 - define root component - red

---
+++ basics/test/root-component.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var RootComponent = require('../lib/root-component.jsx');
+
+describe('Root Component', function () {
+    beforeEach(function () {
+
+    });
+
+    it('should be a unordered list', function () {
+
+    });
+});
