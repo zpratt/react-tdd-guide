057eee5ab80f1f0ad2a7868ece555f958eb28095
zach pratt <zach@attackofzach.com>
2015-04-19 12:05:56 -0500

    step 1 - force it to render something - red

--- basics/test/root-component.spec.js
+++ basics/test/root-component.spec.js
@@ -2,14 +2,17 @@
 
 var RootComponent = require('../lib/root-component.jsx'),
 
-    React = require('react');
+    React = require('react'),
+    ReactTestUtils = require('react/lib/ReactTestUtils');
 
 describe('Root Component', function () {
+    var rootElement;
+
     beforeEach(function () {
-        React.createElement(RootComponent);
+        rootElement = React.createElement(RootComponent);
     });
 
     it('should be a unordered list', function () {
-
+        ReactTestUtils.renderIntoDocument(rootElement);
     });
 });
