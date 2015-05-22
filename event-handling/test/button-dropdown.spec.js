1fb19a74fd4b5d3f5505e5d0c60c13066c693543
zach pratt <zach@attackofzach.com>
2015-05-21 22:47:43 -0500

    red - provide a parent click handler as a property that should be notified when the button is clicked

--- event-handling/test/button-dropdown.spec.js
+++ event-handling/test/button-dropdown.spec.js
@@ -5,19 +5,43 @@ var ButtonDropdownComponent = require('../lib/button-dropdown-component.jsx'),
     React = require('react'),
     ReactTestUtils = require('react/lib/ReactTestUtils'),
 
+    sinon = require('sinon'),
     expect = require('chai').expect;
 
 describe('Button Dropdown', function () {
-    it('should be a button', function () {
-        var buttonDropdownElement = React.createElement(ButtonDropdownComponent),
-            renderedElement,
-            renderedNode;
+    var buttonDropdownElement,
+        renderedElement,
+        renderedNode,
+
+        parentEventHandler,
+
+        sandbox;
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+        parentEventHandler = sandbox.stub();
+
+        buttonDropdownElement = React.createElement(ButtonDropdownComponent, {
+            buttonSelected: parentEventHandler
+        });
 
         renderedElement = ReactTestUtils.renderIntoDocument(buttonDropdownElement);
 
         renderedNode = React.findDOMNode(renderedElement);
+    });
 
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    it('should be a button', function () {
         expect(renderedNode.tagName).to.equal('BUTTON');
         expect(renderedNode.type).to.equal('button');
+    });
+
+    it('should notify a consumer that the button was clicked', function () {
+        ReactTestUtils.Simulate.click(renderedNode);
+
+        sinon.assert.calledOnce(parentEventHandler);
     });
 });
