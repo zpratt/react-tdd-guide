0e45b6b707e6ef2185939c984c987f644f03d5aa
rcoy-v <rcoy.vance@gmail.com>
2016-05-31 22:00:39 -0500

    green - assert input value changes as text is entered

--- state/lib/text-input.js
+++ state/lib/text-input.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
 
+function onChange(event) {
+    this.setState({value: event.target.value});
+}
+
 export default class TextInput extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: ''
+        };
+    }
+
     render() {
         return (
             <input
+                onChange={onChange.bind(this)}
                 type='text'
-                value=''
+                value={this.state.value}
             />
         );
     }
