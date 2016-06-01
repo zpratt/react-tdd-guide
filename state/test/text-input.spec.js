716a18ec42cde1efb7934805f58add905b61aecf
rcoy-v <rcoy.vance@gmail.com>
2016-05-31 21:59:58 -0500

    red - assert input value changes as text is entered

--- state/test/text-input.spec.js
+++ state/test/text-input.spec.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from 'chai';
+import Chance from 'chance';
 import TextInput from '../lib/text-input';
 
 describe('Stateful Text Input', function () {
-    let textInput;
+    let textInput,
+        chance;
 
     beforeEach(function () {
+        chance = new Chance();
         textInput = shallow(<TextInput/>);
     });
 
@@ -17,5 +20,18 @@ describe('Stateful Text Input', function () {
 
     it('should be empty by default', function () {
         expect(textInput.props().value).equals('');
+    });
+
+    it('should update its value when text is entered', function () {
+        const newInputText = chance.string(),
+            onChangeEvent = {
+                target: {
+                    value: newInputText
+                }
+            };
+
+        textInput.simulate('change', onChangeEvent);
+
+        expect(textInput.props().value).equals(newInputText);
     });
 });
