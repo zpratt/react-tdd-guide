b0944633f695070afe67953cacccc6ec467fbc62
rcoy-v <rcoy.vance@gmail.com>
2016-05-31 21:28:48 -0500

    green - assert stateful text input is text input element

---
+++ state/lib/text-input.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class TextInput extends Component {
+    render() {
+        return (
+            <input type='text'/>
+        );
+    }
+}
